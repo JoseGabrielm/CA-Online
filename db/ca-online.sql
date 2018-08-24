c41fea9d77bcce2a73dc090c5030ca311b08f7da
vitor@vitor.com <vitor@vitor.com>
2018-08-24 12:27:52 -0300

    Signed-off-by: vitor@vitor.com <vitor@vitor.com>

--- db/ca-online.sql
+++ db/ca-online.sql
@@ -1,39 +1,28 @@
-drop database caonline;
 create database caonline;
 use caonline;
 
 create table registros(  
-    id_registro int primary key AUTO_INCREMENT,
-    nome varchar(255),
-    username varchar(255),
+    id int primary key AUTO_INCREMENT,
+    name varchar(255),
     email varchar(255),
     password varchar(255),
     cpf varchar(255),
-    nivel_acesso int not null
+    nivel_acesso int default 0,
+    created_at  date,
+    updated_at  date
 );
 create table aplicadores(
-    id_aplicador int primary key AUTO_INCREMENT,
+    id int primary key AUTO_INCREMENT,
     id_posto int, 
  	id_registro int,
     endereco varchar(255),
-    foreign key(id_registro) references  registros(id_registro)
+    foreign key(id_registro) references  registros(id)
 
 );
-create table usuarios(
-    id_usuario int PRIMARY KEY AUTO_INCREMENT,
-    id_registro int,
-    foreign key(id_registro) references  registros(id_registro)
-);
-create table admins(
-    id_admin int primary key AUTO_INCREMENT,
-    id_registro int,
 
-    
-    foreign key(id_registro) references  registros(id_registro)
-);
 
 create table postos(
-    id_posto int PRIMARY KEY AUTO_INCREMENT,
+    id int PRIMARY KEY AUTO_INCREMENT,
     cidade varchar(255),
     endereco varchar(255),
     estado varchar(255)
@@ -44,17 +33,18 @@ create table vacinas(
   	lote int,
     tipo int,
     data_de_validade date,
-    data_adicionado date,
-    data_atualizado date
+    created_at  date,
+    updated_at  date
 );
 
 create table historico_vacinas(
     id_vacina int,
- 	id_usuario int,
+ 	id_registro int,
     id_aplicador int,
-    data_aplicacao date,
+    created_at  date,
+    updated_at  date,
 
 
-    foreign key(id_usuario) references  usuarios(id_usuario),  
-    foreign key(id_aplicador) references aplicadores(id_aplicador)
+    foreign key(id_registro) references  registros(id),  
+    foreign key(id_aplicador) references aplicadores(id)
 );
